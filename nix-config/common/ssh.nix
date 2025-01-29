b20d92156b52067c4b410b16b0bb604b9aeb8909
Jade Meskill <jade.isaiah@gmail.com>
2025-01-29 11:13:43 -0700

    Change "*" user to "root" in ssh.nix

--- nix-config/common/ssh.nix
+++ nix-config/common/ssh.nix
@@ -11,7 +11,7 @@
     };
   };
 
-  users.users."*".openssh.authorizedKeys.keys = [
+  users.users."root".openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINFYN8DlBKCTbFYgl52VFImP58YRBaYdPTKuyaY6e37T"
   ];
 }
