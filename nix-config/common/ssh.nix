c0139710d072913bdd8d255e27808ab766da34ae
Jade Meskill <jade.isaiah@gmail.com>
2025-01-29 11:12:30 -0700

    Change "8" user to "*" in ssh.nix

--- nix-config/common/ssh.nix
+++ nix-config/common/ssh.nix
@@ -11,7 +11,7 @@
     };
   };
 
-  users.users."8".openssh.authorizedKeys.keys = [
+  users.users."*".openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINFYN8DlBKCTbFYgl52VFImP58YRBaYdPTKuyaY6e37T"
   ];
 }
