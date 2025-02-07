7ad2609468805e16d0c0e5c4fc740c6a10f0fdf2
Jade Meskill <jade.isaiah@gmail.com>
2025-02-07 16:02:47 -0700

    change flake.nix

--- nix-config/disko-config/flake.nix
+++ nix-config/disko-config/flake.nix
@@ -1,41 +1,51 @@
 {
-  description = "";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  inputs.disko.url = "github:nix-community/disko/latest";
+  inputs.disko.inputs.nixpkgs.follows = "nixpkgs";
 
-  inputs = {
-    disko = {
-      url = "github:nix-community/disko/latest";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
-
-  outputs = { 
-    self, 
-    disko, 
-    nixpkgs,
-    ...
-    } @ inputs: 
-    let
-      layoutModules = {
-        single-ext4 = [
-          ./layouts/single-ext4.nix
-        ];
-      };
-
-      nixosConfiguration = layout:
-        nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-
-          modules = [
-            /tmp/config/configuration.nix
-            disko.nixosModules.disko
-          ] ++ layoutModules.${layout};
+  outputs = { self, disko, nixpkgs }: {
+    nixosConfigurations.mymachine = nixpkgs.legacyPackages.x86_64-linux.nixos [
+      ./configuration.nix
+      disko.nixosModules.disko
+      {
+        disko.devices = {
+          disk = {
+            main = {
+              # When using disko-install, we will overwrite this value from the commandline
+              device = "/dev/disk/by-id/some-disk-id";
+              type = "disk";
+              content = {
+                type = "gpt";
+                partitions = {
+                  MBR = {
+                    type = "EF02"; # for grub MBR
+                    size = "1M";
+                    priority = 1; # Needs to be first partition
+                  };
+                  ESP = {
+                    type = "EF00";
+                    size = "500M";
+                    content = {
+                      type = "filesystem";
+                      format = "vfat";
+                      mountpoint = "/boot";
+                      mountOptions = [ "umask=0077" ];
+                    };
+                  };
+                  root = {
+                    size = "100%";
+                    content = {
+                      type = "filesystem";
+                      format = "ext4";
+                      mountpoint = "/";
+                    };
+                  };
+                };
+              };
+            };
+          };
         };
-    in 
-    {
-      nixosConfigurations = {
-        "single-ext4" = nixosConfiguration "single-ext4";
-      };
-    };
+      }
+    ];
+  };
 }
