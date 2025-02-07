85d2ba35afb288f45ac1148a656a0c2ff8b56891
Jade Meskill <jade.isaiah@gmail.com>
2025-02-06 23:10:49 -0700

    adjust README.md and add separate disko.nix flake

--- nix-config/disko.nix
+++ nix-config/disko.nix
@@ -1,43 +1,71 @@
-{ 
-  diskName, 
-  ...
-}:
-
 {
-  disko.devices = {
-    disk = {
-      main = {
-        type = "disk";
-        content = {
-          type = "gpt";
-          partitions = {
-            boot = {
-              size = "1M";
-              type = "EF02";
-            };
-            
-            ESP = {
-              size = "1G";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = [ "umask=0077" ];
-              };
-            };
+  description = "";
 
-            root = {
-              size = "100%";
-              content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
-              };
-            };
-          };
+  inputs = {
+    disko = {
+      url = "github:nix-community/disko/latest";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  };
+
+  outputs = {
+    self, 
+    disko, 
+    nixpkgs, 
+    ...
+    } @ inputs:
+    let
+      nixosConfiguration = layout:
+        nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+
+          modules = [
+            disko.nixosModules.disko {
+              disko.devices = {
+                  disk = {
+                    main = {
+                      type = "disk";
+                      content = {
+                        type = "gpt";
+                        partitions = {
+                          boot = {
+                            size = "1M";
+                            type = "EF02";
+                          };
+                          
+                          ESP = {
+                            size = "1G";
+                            type = "EF00";
+                            content = {
+                              type = "filesystem";
+                              format = "vfat";
+                              mountpoint = "/boot";
+                              mountOptions = [ "umask=0077" ];
+                            };
+                          };
+
+                          root = {
+                            size = "100%";
+                            content = {
+                              type = "filesystem";
+                              format = "ext4";
+                              mountpoint = "/";
+                            };
+                          };
+                        };
+                      };
+                    };
+                  };
+                };
+            }
+          ];
         };
+    in
+    {
+      nixosConfigurations = {
+        "single_ext4" = nixosConfiguration "single_ext4";
       };
     };
-  };
 }
