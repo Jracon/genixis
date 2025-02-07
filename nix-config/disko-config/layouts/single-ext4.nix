b7dab6388f9892fae93ed85f6fcd3d92f457fda8
Jade Meskill <jade.isaiah@gmail.com>
2025-02-07 14:18:29 -0700

    test enableConfig

--- nix-config/disko-config/layouts/single-ext4.nix
+++ nix-config/disko-config/layouts/single-ext4.nix
@@ -1,34 +1,38 @@
 {
-  disko.devices = {
-    disk = {
-      main = {
-        device = "/foo/bar";
-        type = "disk";
-        content = {
-          type = "gpt";
-          partitions = {
-            boot = {
-              size = "1M";
-              type = "EF02";
-            };
-
-            ESP = {
-              size = "1G";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = [ "umask=0077" ];
+  disko = {
+    enableConfig = false;
+    
+    devices = {
+      disk = {
+        main = {
+          device = "/foo/bar";
+          type = "disk";
+          content = {
+            type = "gpt";
+            partitions = {
+              boot = {
+                size = "1M";
+                type = "EF02";
               };
-            };
 
-            root = {
-              size = "100%";
-              content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
+              ESP = {
+                size = "1G";
+                type = "EF00";
+                content = {
+                  type = "filesystem";
+                  format = "vfat";
+                  mountpoint = "/boot";
+                  mountOptions = [ "umask=0077" ];
+                };
+              };
+
+              root = {
+                size = "100%";
+                content = {
+                  type = "filesystem";
+                  format = "ext4";
+                  mountpoint = "/";
+                };
               };
             };
           };
