5db360aabf5525fba34fd0c0652f882c18dcd519
Jade Meskill <jade.isaiah@gmail.com>
2025-02-09 11:50:04 -0700

    disable disko config

--- nix-config/disk-layouts/single-ext4.nix
+++ nix-config/disk-layouts/single-ext4.nix
@@ -7,36 +7,39 @@ let
   primaryDisk = builtins.elemAt disks 0;
 in
 {
-  disko.devices = {
-    disk = {
-      main = {
-        device = primaryDisk;
-        type = "disk";
-        content = {
-          type = "gpt";
-          partitions = {
-            boot = {
-              size = "1M";
-              type = "EF02";
-            };
-
-            ESP = {
-              size = "1G";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = [ "umask=0077" ];
+  disko = {
+    enableConfig = false;
+    devices = {
+      disk = {
+        main = {
+          device = primaryDisk;
+          type = "disk";
+          content = {
+            type = "gpt";
+            partitions = {
+              boot = {
+                size = "1M";
+                type = "EF02";
               };
-            };
 
-            root = {
-              size = "100%";
-              content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
+              ESP = {
+                size = "1G";
+                type = "EF00";
+                content = {
+                  type = "filesystem";
+                  format = "vfat";
+                  mountpoint = "/boot";
+                  mountOptions = [ "umask=0077" ];
+                };
+              };
+
+              root = {
+                size = "100%";
+                content = {
+                  type = "filesystem";
+                  format = "ext4";
+                  mountpoint = "/";
+                };
               };
             };
           };
