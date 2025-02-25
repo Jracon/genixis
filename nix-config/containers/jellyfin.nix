45d70fb943a6b88ed86ec7b5dd3e33c9620d9b97
Jade Meskill <jade.isaiah@gmail.com>
2025-02-24 23:01:57 -0700

    Add comments to jellyfin.nix, remove jellyfin from docker configuration.

--- nix-config/containers/jellyfin.nix
+++ nix-config/containers/jellyfin.nix
@@ -8,18 +8,18 @@
       hostname = "jellyfin";
       image = "lscr.io/linuxserver/jellyfin:latest";
 
-      # devices = [ "/dev/dri:/dev/dri" ];
+      # devices = [ "/dev/dri:/dev/dri" ]; # waiting for next release
       environment = {
         TZ="America/Phoenix";
       };
-      extraOptions = [ "--restart=unless-stopped" "--device=/dev/dri:/dev/dri" ];
+      extraOptions = [ "--restart=unless-stopped" "--device=/dev/dri:/dev/dri" "--pull=always" ]; # device and pull options are temporary
       ports = [ 
         "8096" 
         "8920" 
         "7359/udp" 
         "1900/udp"
       ];
-      # pull = "always";
+      # pull = "always"; # waiting for next release
       user = "1000:1000";
       volumes = [ 
         "/mnt/media/data/jellyfin:/config" 
