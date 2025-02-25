f5e5b66b8ebda888bef0dba6f87187d7d20ec9c9
Jade Meskill <jade.isaiah@gmail.com>
2025-02-25 16:01:56 -0700

    update incus.nix to allow ui through firewall, elaborate on preseed

--- nix-config/roles/incus.nix
+++ nix-config/roles/incus.nix
@@ -7,9 +7,22 @@ let
   primaryInterface = builtins.elemAt interfaces 0;
 in
 {
+  networking.firewall.allowedTCPPorts = [ 8443 ];
+  
   virtualisation.incus = {
     enable = true;
     preseed = {
+      config = {
+        core.https_address = ":8443";
+      };
+
+      storage_pools = [
+        {
+          driver = "btrfs";
+          name = "default";
+        }
+      ];
+
       networks = [
         {
           name = "imv0";
@@ -25,6 +38,13 @@ in
           name = "default";
 
           devices = {
+            dri = {
+              name = "dri";
+              type = "disk";
+              source = "/dev/dri";
+              path = "/dev/dri";
+            };
+
             eth0 = {
               name = "eth0";
               network = "imv0";
@@ -39,15 +59,8 @@ in
           };
         }
       ];
-
-      storage_pools = [
-        {
-          driver = "btrfs";
-          name = "default";
-        }
-      ];
     };
-    
+  
     ui.enable = true;
   };
 
