f11fe20f26372517797a1bcf70fa4e17e2471708
Jade Meskill <jade.isaiah@gmail.com>
2025-02-24 22:50:12 -0700

    revert previous commit, add device as extra option in jellyfin.nix

--- nix-config/containers/jellyfin.nix
+++ nix-config/containers/jellyfin.nix
@@ -1,19 +1,18 @@
 {
-  config, 
   ...
 }:
 
 {
-  config.virtualisation.oci-containers.containers = {
+  virtualisation.oci-containers.containers = {
     jellyfin = {
       hostname = "jellyfin";
       image = "lscr.io/linuxserver/jellyfin:latest";
 
-      devices = [ "/dev/dri:/dev/dri" ];
+      # devices = [ "/dev/dri:/dev/dri" ];
       environment = {
         TZ="America/Phoenix";
       };
-      extraOptions = [ "--restart=unless-stopped" ];
+      extraOptions = [ "--restart=unless-stopped" "--device=/dev/dri:/dev/dri" ];
       ports = [ 
         "8096" 
         "8920" 
