0602cc4103119b4e42d2a0c02f97937300958713
Jade Meskill <jade.isaiah@gmail.com>
2025-02-25 21:09:57 -0700

    alter jellyfin primary port

--- nix-config/containers/jellyfin.nix
+++ nix-config/containers/jellyfin.nix
@@ -15,19 +15,16 @@
 
       devices = [ "/dev/dri:/dev/dri" ];
       environment = {
-        PUID="1000";
-        PGID="1000";
         TZ="America/Phoenix";
       };
-      # extraOptions = [ "--restart=unless-stopped" ];
       ports = [ 
-        "8096" 
+        "8097" 
         "8920" 
         "7359/udp" 
         "1900/udp"
       ];
       pull = "always";
-      # user = "1000:1000";
+      user = "1000:1000";
       volumes = [ 
         "/mnt/media/data/jellyfin:/config" 
         "/mnt/media:/mnt/media" 
