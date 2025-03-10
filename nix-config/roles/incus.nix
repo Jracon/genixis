eb6fbb0bd6becc0104f640b47bfb01b6f44dc999
Jade Meskill <jade.isaiah@gmail.com>
2025-03-09 18:49:23 -0700

    reimplement interface input for incus bridge

--- nix-config/roles/incus.nix
+++ nix-config/roles/incus.nix
@@ -1,78 +1,86 @@
 {
+  devices, 
   ...
 }:
 
-{
-  # enable Incus (and the UI) and set preseed values
-  virtualisation.incus = {
-    enable = true;
-    preseed = {
-      config = {
-        "core.https_address" = ":8443";
-      };
+let
+  primaryInterface = builtins.elemAt devices.interfaces 0;
+in
+  {
+    # enable Incus (and the UI) and set preseed values
+    virtualisation.incus = {
+      enable = true;
+      preseed = {
+        config = {
+          "core.https_address" = ":8443";
+        };
 
-      networks = [
-        {
-          name = "ib0";
-          type = "bridge";
-        }
-      ];
+        networks = [
+          {
+            name = "ib0";
+            type = "bridge";
 
-      profiles = [
-        {
-          name = "default";
+            config = {
+              "external_interfaces" = primaryInterface;
+            };
+          }
+        ];
 
-          config = {
-            "security.nesting" = "true";
-          };
+        profiles = [
+          {
+            name = "default";
 
-          devices = (
-            if 
-              builtins.pathExists "/dev/dri" 
-            then 
+            config = {
+              "security.nesting" = "true";
+            };
+
+            devices = (
+              if 
+                builtins.pathExists "/dev/dri" 
+              then 
+                {
+                  dri = {
+                    type = "disk";
+                    source = "/dev/dri";
+                    path = "/dev/dri";
+                  };
+                }
+              else
+                { }
+              ) 
+              // 
               {
-                dri = {
-                  type = "disk";
-                  source = "/dev/dri";
-                  path = "/dev/dri";
-                };
-              }
-            else
-              { }
-            ) 
-            // 
-            {
-            eth0 = {
-              name = "eth0";
-              type = "nic";
-              network = "ib0";
+              eth0 = {
+                name = "eth0";
+                type = "nic";
+                network = "ib0";
+              };
+
+              root = {
+                type = "disk";
+                path = "/";
+                pool = "default";
+              };
             };
+          }
+        ];
 
-            root = {
-              type = "disk";
-              path = "/";
-              pool = "default";
-            };
-          };
-        }
-      ];
-
-      storage_pools = [
-        {
-          name = "default";
-          driver = "btrfs";
-        }
-      ];
-    };
-  
-    ui.enable = true;
-  };
-
-  networking = {
-    firewall.allowedTCPPorts = [ 
-      8443 
-    ];
+        storage_pools = [
+          {
+            name = "default";
+            driver = "btrfs";
+          }
+        ];
+      };
     
-    nftables.enable = true;
-  };
-}
+      ui.enable = true;
+    };
+
+    networking = {
+      firewall.allowedTCPPorts = [ 
+        8443 
+      ];
+      
+      nftables.enable = true;
+    };
+  }
