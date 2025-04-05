f13a236f1af8ef9e94edfa35c1f9a617c734e78d
Jade Meskill <jade.isaiah@gmail.com>
2025-04-04 23:34:58 -0700

    try to fix helper script

--- nix-config/disk-layouts/.helper.nix
+++ nix-config/disk-layouts/.helper.nix
@@ -5,9 +5,12 @@
 
 {
   system.activationScripts = {
-    helper.text = ''
-      grep -q 'disk-layout = "${layout}";' /tmp/etc/nixos/local.nix || sed -i '/^}/i\  disk-layout = "${layout}";' /tmp/etc/nixos/local.nix
-      cp -r /tmp/etc /mnt
-    '';
+    helper = {
+      deps = [ "specialfs" ];
+      text = ''
+        grep -q 'disk-layout = "${layout}";' /tmp/etc/nixos/local.nix || sed -i '/^}/i\  disk-layout = "${layout}";' /tmp/etc/nixos/local.nix
+        cp -r /tmp/etc /mnt
+      '';
+    };
   };
 }
