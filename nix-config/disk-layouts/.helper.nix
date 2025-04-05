cd32343e643ccb6c1b9fba65a30048cadc899b6d
Jade Meskill <jade.isaiah@gmail.com>
2025-04-04 23:46:53 -0700

    fix

--- nix-config/disk-layouts/.helper.nix
+++ nix-config/disk-layouts/.helper.nix
@@ -8,8 +8,8 @@
     helper = {
       deps = [ "specialfs" ];
       text = ''
-        grep -q 'disk-layout = "${layout}";' /home/nixos/etc/nixos/local.nix || sed -i '/^}/i\  disk-layout = "${layout}";' /home/nixos/etc/nixos/local.nix
-        cp -r /tmp/etc /mnt
+        grep -q 'disk-layout = "${layout}";' /tmp/etc/nixos/local.nix || sed -i '/^}/i\  disk-layout = "${layout}";' /tmp/etc/nixos/local.nix
+        cp -r /home/nixos/etc /mnt
       '';
     };
   };
