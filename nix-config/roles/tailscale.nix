dbee084c4735bcab7de63fde3405be8d0fe68e14
Jade Meskill <jade.isaiah@gmail.com>
2025-04-14 20:07:04 -0700

    fix tailscale up script

--- nix-config/roles/tailscale.nix
+++ nix-config/roles/tailscale.nix
@@ -1,5 +1,6 @@
 {
   config, 
+  pkgs, 
   ...
 }:
 
@@ -15,13 +16,8 @@
 
   system.activationScripts = {
     tailscale_up.text = ''
-    #!/bin/sh
-
-    # Read the decrypted secret from the file
-    AUTH_KEY=$(cat ${config.age.secrets.tailscale_client_secret.path})
-
-    # Bring up Tailscale with the key
-    /run/wrappers/bin/tailscale up --auth-key="$AUTH_KEY?ephemeral=false" --advertise-tags=tag:genixis
+      AUTH_KEY=$(cat ${config.age.secrets.tailscale_client_secret.path})
+      ${pkgs.tailscale} up --auth-key="$AUTH_KEY?ephemeral=false" --advertise-tags=tag:genixis
   '';
   };
 }
