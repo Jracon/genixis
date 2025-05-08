11772ea73c5169b824229b1dbcde8e92309cee4d
Jade Meskill <jade.isaiah@gmail.com>
2025-05-07 19:31:25 -0700

    fix script name

--- modules/services/tailscale.nix
+++ modules/services/tailscale.nix
@@ -12,7 +12,7 @@
     useRoutingFeatures = "server";
   };
 
-  system.activationScripts.ailscale_up.text = ''
+  system.activationScripts.tailscale_up.text = ''
     TAILSCALE_CLIENT_SECRET=$(cat ${config.age.secrets.tailscale_client_secret.path})
     ${pkgs.tailscale}/bin/tailscale up --auth-key="$TAILSCALE_CLIENT_SECRET?ephemeral=false" --advertise-tags=tag:genixis
   '';
