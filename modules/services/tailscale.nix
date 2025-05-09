f056c3f111f5c2c47c1061b4f3df228ffcdcef36
Jade Meskill <jade.isaiah@gmail.com>
2025-05-08 23:53:32 -0700

    remove old tailscale activation script

--- modules/services/tailscale.nix
+++ modules/services/tailscale.nix
@@ -17,10 +17,4 @@
     openFirewall = true;
     useRoutingFeatures = "server";
   };
-
-  # system.activationScripts.tailscale_up.text = ''
-  #   ${pkgs.tailscale}/bin/tailscaled -cleanup
-  #   TAILSCALE_CLIENT_SECRET=$(cat ${config.age.secrets.tailscale_client_secret.path})
-  #   ${pkgs.tailscale}/bin/tailscale up --auth-key="$TAILSCALE_CLIENT_SECRET?ephemeral=false" --advertise-tags=tag:genixis
-  # '';
 }
