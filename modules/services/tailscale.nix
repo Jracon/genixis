598b0e46829f3f0aea0bef04e88112e4ffba1b9b
Jade Meskill <jade.isaiah@gmail.com>
2025-05-08 23:48:20 -0700

    try new tailscale config

--- modules/services/tailscale.nix
+++ modules/services/tailscale.nix
@@ -8,13 +8,15 @@
   age.secrets.tailscale_client_secret.file = ./tailscale/client_secret.age;
 
   services.tailscale = {
+    authKeyFile = config.age.secrets.tailscale_client_secret.path;
     enable = true;
+    openFirewall = true;
     useRoutingFeatures = "server";
   };
 
-  system.activationScripts.tailscale_up.text = ''
-    ${pkgs.tailscale}/bin/tailscaled
-    TAILSCALE_CLIENT_SECRET=$(cat ${config.age.secrets.tailscale_client_secret.path})
-    ${pkgs.tailscale}/bin/tailscale up --auth-key="$TAILSCALE_CLIENT_SECRET?ephemeral=false" --advertise-tags=tag:genixis
-  '';
+  # system.activationScripts.tailscale_up.text = ''
+  #   ${pkgs.tailscale}/bin/tailscaled -cleanup
+  #   TAILSCALE_CLIENT_SECRET=$(cat ${config.age.secrets.tailscale_client_secret.path})
+  #   ${pkgs.tailscale}/bin/tailscale up --auth-key="$TAILSCALE_CLIENT_SECRET?ephemeral=false" --advertise-tags=tag:genixis
+  # '';
 }
