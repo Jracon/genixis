6f488787dc072c15072ba8be61802c7f01d943f2
Jade Meskill <jade.isaiah@gmail.com>
2025-05-19 17:09:24 -0700

    try in conjunction with environment variable

--- modules/containers/media-servers/invidious.nix
+++ modules/containers/media-servers/invidious.nix
@@ -43,9 +43,9 @@ in
         dependsOn = [
           "invidious_db"
         ];
-        # environment = {
-        #   INVIDIOUS_CONFIG_FILE = "${config.age.secrets.invidious_environment.path}";
-        # };
+        environment = {
+          INVIDIOUS_CONFIG_FILE = "/config/config.yml";
+        };
         ports = [
           "3000:3000"
         ];
