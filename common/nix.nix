055826bd1a9c46ed79272662e13f7eba95521c7a
Jade Meskill <jade.isaiah@gmail.com>
2025-08-26 11:57:49 -0700

    update nix config

--- common/nix.nix
+++ common/nix.nix
@@ -3,11 +3,12 @@
 }:
 
 {
-  nixpkgs.config.allowUnsupportedSystem = true;
-  
+  services.nix-daemon.enable = true;
+
   # automatically optimise the Nix store and enable automatic garbage collection
   nix = {
     gc.automatic = true;
     optimise.automatic = true;
+    settings.optimise-store = true;
   };
 }
