9e1a1dd35df9f85ff6aff5d5f925b72aac0b9f6f
Jade Meskill <jade.isaiah@gmail.com>
2025-08-29 16:19:24 -0700

    fix symlinks

--- modules/programs/gui/vscode.nix
+++ modules/programs/gui/vscode.nix
@@ -1,8 +1,13 @@
 {
+  pkgs,
   ...
 }:
 
 {
+  environment.systemPackages = with pkgs; [
+    vscode
+  ];
+
   programs.vscode = {
     enable = true;
   };
--- users/jademeskill.nix
+++ users/jademeskill.nix
@@ -6,5 +6,4 @@
   programs.home-manager.enable = true;
 
   nixpkgs.config.allowUnfree = true;
-  targets.darwin.linkApps.enable = true;
 }
