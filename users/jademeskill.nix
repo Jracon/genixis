30e2505719b92e562de18cecc92d745f51810c86
Jade Meskill <jade.isaiah@gmail.com>
2025-08-29 17:52:08 -0700

    fix linkApps

--- users/jademeskill.nix
+++ users/jademeskill.nix
@@ -4,10 +4,7 @@
 
 {
   programs.home-manager.enable = true;
-  nixpkgs.config.allowUnfree = true;
 
-  targets.darwin.linkApps = {
-    enable = true;
-    directory = "Home Manager Apps";
-  };
+  nixpkgs.config.allowUnfree = true;
+  targets.darwin.linkApps.enable = true;
 }
