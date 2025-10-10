d44fc4b20a9ba179a80eddd984a7b543e986fb23
Jade Isaiah Meskill <jade.isaiah@gmail.com>
2025-10-09 17:45:27 -0700

    fix finder kill

--- modules/programs/darwin/make-app-trampolines.sh
+++ modules/programs/darwin/make-app-trampolines.sh
@@ -29,8 +29,6 @@ mkdir -p "$toDir"
     fi
   done
 
-  /System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister \
-    -kill -r -domain local -domain system -domain user
   killall Finder
 )
 
