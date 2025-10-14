c3daeba7e9c72755514507a604e55423f9eb3592
Jade Isaiah Meskill <jade.isaiah@gmail.com>
2025-10-14 11:46:58 -0700

    update tmux

--- modules/programs/cli/tmux.nix
+++ modules/programs/cli/tmux.nix
@@ -8,6 +8,7 @@
     enable = true;
     baseIndex = 1;
     disableConfirmationPrompt = true;
+    escapeTime = 200;
     keyMode = "vi";
     mouse = true; # TODO based on local.gui or isDarwin?
     newSession = true;
