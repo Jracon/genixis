89327cdae8b304898bae03a35575a417b5947428
Jade Isaiah Meskill <jade.isaiah@gmail.com>
2025-10-22 16:43:03 -0700

    force sonarr inclusion

--- templates/node.nix
+++ templates/node.nix
@@ -36,12 +36,63 @@ let
       {
         imports = [
           ../modules/podman.nix
-          ../modules/containers/media-managers/sonarr.nix
         ];
 
         networking = {
           useDHCP = lib.mkForce true;
           firewall.enable = true;
+          firewall.allowedTCPPorts = [
+            8989
+            8990
+          ];
+        };
+
+        system.activationScripts.create_sonarr_directories.text = ''
+          mkdir -p /mnt/sonarr /mnt/media /mnt/sonarr-anime
+        '';
+
+        virtualisation.oci-containers.containers = {
+          sonarr = {
+            image = "lscr.io/linuxserver/sonarr:latest";
+            pull = "newer";
+            hostname = "sonarr";
+
+            environment = {
+              PUID = "1000";
+              PGID = "1000";
+              TZ = "America/Phoenix";
+            };
+
+            volumes = [
+              "/mnt/sonarr:/config"
+              "/mnt/media:/mnt/media"
+            ];
+
+            ports = [
+              "8989:8989"
+            ];
+          };
+
+          sonarr-anime = {
+            image = "lscr.io/linuxserver/sonarr:latest";
+            pull = "newer";
+            hostname = "sonarr-anime";
+
+            environment = {
+              PUID = "1000";
+              PGID = "1000";
+              TZ = "America/Phoenix";
+            };
+
+            volumes = [
+              "/mnt/sonarr-anime:/config"
+              "/mnt/media:/mnt/media"
+            ];
+
+            ports = [
+              "8990:8989"
+            ];
+          };
         };
       };
   };
