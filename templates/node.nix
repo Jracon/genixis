7140cfb2f21fa94d5c9d7f27a418642c4503a43a
Jade Isaiah Meskill <jade.isaiah@gmail.com>
2025-10-22 17:45:57 -0700

    remove sonarr container

--- templates/node.nix
+++ templates/node.nix
@@ -44,7 +44,7 @@ let
       {
         imports = [
           ../modules/podman.nix
-        ];
+        ] ++ generateContainerModules container;
 
         networking = {
           useDHCP = lib.mkForce true;
@@ -53,54 +53,6 @@ let
             8989
             8990
           ];
-        };
-
-        system.activationScripts.create_sonarr_directories.text = ''
-          mkdir -p /mnt/sonarr /mnt/media /mnt/sonarr-anime
-        '';
-
-        virtualisation.oci-containers.containers = {
-          sonarr = {
-            image = "lscr.io/linuxserver/sonarr:latest";
-            pull = "newer";
-            hostname = "sonarr";
-
-            environment = {
-              PUID = "1000";
-              PGID = "1000";
-              TZ = "America/Phoenix";
-            };
-
-            volumes = [
-              "/mnt/sonarr:/config"
-              "/mnt/media:/mnt/media"
-            ];
-
-            ports = [
-              "8989:8989"
-            ];
-          };
-
-          sonarr-anime = {
-            image = "lscr.io/linuxserver/sonarr:latest";
-            pull = "newer";
-            hostname = "sonarr-anime";
-
-            environment = {
-              PUID = "1000";
-              PGID = "1000";
-              TZ = "America/Phoenix";
-            };
-
-            volumes = [
-              "/mnt/sonarr-anime:/config"
-              "/mnt/media:/mnt/media"
-            ];
-
-            ports = [
-              "8990:8989"
-            ];
-          };
         };
       };
   };
