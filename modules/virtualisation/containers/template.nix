790382c638931eb1866de629f5adbf9b549672d1
Jade Isaiah Meskill <jade.isaiah@gmail.com>
2025-10-28 09:27:40 -0700

    fix template.nix

--- modules/virtualisation/containers/template.nix
+++ modules/virtualisation/containers/template.nix
@@ -111,54 +111,56 @@ let
     };
 in
 {
+  config = {
+    networking = {
+      bridges.br0.interfaces = [ primaryInterface ];
+      useNetworkd = true;
+
+      interfaces = {
+        "${primaryInterface}".useDHCP = false;
+        br0.useDHCP = true;
+      };
+    };
+
+    systemd.services.set-br0-mac = {
+      after = [
+        "systemd-networkd.service"
+        "sys-subsystem-net-devices-${primaryInterface}.device"
+        "network-online.target"
+      ];
+
+      path = [
+        "/run/current-system/sw"
+      ];
+
+      requires = [
+        "systemd-networkd.service"
+      ];
+
+      serviceConfig = {
+        Type = "oneshot";
+
+        ExecStart = pkgs.writeShellScript "set-br0-mac" ''
+          mac=$(cat /sys/class/net/${primaryInterface}/address)
+          ip link set dev br0 down
+          ip link set dev br0 address "$mac"
+          ip link set dev br0 up
+        '';
+      };
+
+      wantedBy = [
+        "multi-user.target"
+      ];
+
+      wants = [
+        "network-online.target"
+      ];
+    };
+  };
+
   config._module.args = {
     containerTemplate = {
       inherit generateContainer;
     };
-  };
-
-  networking = {
-    bridges.br0.interfaces = [ primaryInterface ];
-    useNetworkd = true;
-
-    interfaces = {
-      "${primaryInterface}".useDHCP = false;
-      br0.useDHCP = true;
-    };
-  };
-
-  systemd.services.set-br0-mac = {
-    after = [
-      "systemd-networkd.service"
-      "sys-subsystem-net-devices-${primaryInterface}.device"
-      "network-online.target"
-    ];
-
-    path = [
-      "/run/current-system/sw"
-    ];
-
-    requires = [
-      "systemd-networkd.service"
-    ];
-
-    serviceConfig = {
-      Type = "oneshot";
-
-      ExecStart = pkgs.writeShellScript "set-br0-mac" ''
-        mac=$(cat /sys/class/net/${primaryInterface}/address)
-        ip link set dev br0 down
-        ip link set dev br0 address "$mac"
-        ip link set dev br0 up
-      '';
-    };
-
-    wantedBy = [
-      "multi-user.target"
-    ];
-
-    wants = [
-      "network-online.target"
-    ];
   };
 }
