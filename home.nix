8db61e148f027994a873e5b31194b6534cd4bf7e
Jade Isaiah Meskill <jade.isaiah@gmail.com>
2025-10-29 15:46:48 -0700

    fix root home directory

--- home.nix
+++ home.nix
@@ -6,7 +6,13 @@
 
 {
   home = {
-    homeDirectory = if pkgs.stdenv.isDarwin then "/Users/${user.name}" else "/home/${user.name}";
+    homeDirectory =
+      if pkgs.stdenv.isDarwin then
+        "/Users/${user.name}"
+      else if user.name == "root" then
+        "/root"
+      else
+        "/home/${user.name}";
     stateVersion = "25.05";
     username = "${user.name}";
   };
