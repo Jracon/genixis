000e44357866cc65f67c4c10e6f2f61e094fb020
Jade Isaiah Meskill <jade.isaiah@gmail.com>
2025-10-29 15:50:49 -0700

    fix zsh

--- common/nixos.nix
+++ common/nixos.nix
@@ -4,12 +4,15 @@
 }:
 
 {
-  programs.nix-ld.enable = true;
   users.defaultUserShell = pkgs.zsh;
 
   boot.loader = {
     generic-extlinux-compatible.configurationLimit = 5;
     grub.configurationLimit = 5;
     systemd-boot.configurationLimit = 5;
+  };
+  programs = {
+    nix-ld.enable = true;
+    zsh.enable = true;
   };
 }
