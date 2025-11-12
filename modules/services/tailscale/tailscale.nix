b90f45cd370ddbfe5edaacc0e65fde683b13f65e
Jade Isaiah Meskill <jade.isaiah@gmail.com>
2025-11-12 13:13:21 -0700

    try ethtool bin

--- modules/services/tailscale/tailscale.nix
+++ modules/services/tailscale/tailscale.nix
@@ -1,6 +1,5 @@
 {
   config,
-  lib,
   pkgs,
   ...
 }:
@@ -23,8 +22,7 @@
         ];
         script = ''
           NETDEV=$(ip -o route get 8.8.8.8 | cut -f 5 -d " ")
-          echo "$NETDEV"
-          ${lib.getExe ethtool} -K "$NETDEV" rx-udp-gro-forwarding on rx-gro-list off
+          ${pkgs.ethtool}/bin/ethtool -K "$NETDEV" rx-udp-gro-forwarding on rx-gro-list off
         '';
       };
     };
