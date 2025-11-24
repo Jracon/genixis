73c3c4aa2d7919b60d243465fe6d7175975876ee
Jade Isaiah Meskill <jade.isaiah@gmail.com>
2025-11-23 18:03:48 -0700

    update calibre, Caddyfile

--- modules/virtualisation/oci-containers/media-servers/calibre.nix
+++ modules/virtualisation/oci-containers/media-servers/calibre.nix
@@ -4,28 +4,28 @@
   ];
 
   system.activationScripts.create_calibre_directories.text = ''
-    mkdir -p /mnt/media/downloads/cwabd /mnt/calibre /mnt/media/calibre-library
+    mkdir -p /mnt/calibre /mnt/media/calibre-library && chown -R 1000:1000 /mnt/calibre /mnt/media/calibre-library
   '';
 
   virtualisation.oci-containers.containers.calibre = {
-    image = "crocodilestick/calibre-web-automated:latest";
+    image = "lscr.io/linuxserver/calibre:latest";
 
-    hostname = "calibre-web-automated";
+    hostname = "calibre";
     pull = "newer";
 
     environment = {
-      DOCKER_MODS = "lscr.io/linuxserver/mods:universal-calibre-v7.16.0";
       PGID = "1000";
       PUID = "1000";
       TZ = "America/Phoenix";
     };
     ports = [
-      "8081:8083"
+      "8081:8080"
+      "8082:8081"
+      "8181:8181"
     ];
     volumes = [
       "/mnt/calibre:/config"
       "/mnt/media/calibre-library:/calibre-library"
-      "/mnt/media/downloads/cwabd:/cwa-book-ingest"
     ];
   };
 }
