a70d1ea6eaf91d31b7510ff2c8a29544e06a065f
Jade Isaiah Meskill <jade.isaiah@gmail.com>
2026-02-06 10:29:11 -0700

    update dummy configuration, flake

--- common/dummy-configuration.nix
+++ common/dummy-configuration.nix
@@ -1,13 +1,16 @@
 {
   ...
 }:
+
 {
   boot.loader.systemd-boot.enable = true;
+
+  networking.hostId = "12345678";
+
+  system.stateVersion = "26.05";
 
   fileSystems."/" = {
     device = "/dev/disk/dummy";
     fsType = "ext4";
   };
-
-  networking.hostId = "12345678";
 }
