1f420dc802f821b1910d6551b6a3519392c97659
Jade Isaiah Meskill <jade.isaiah@gmail.com>
2026-02-06 14:24:47 -0700

    add treefmt

--- common/nix.nix
+++ common/nix.nix
@@ -8,8 +8,9 @@
   nixpkgs.config.allowUnfree = true;
 
   # enable nixfmt
-  environment.systemPackages = [
-    pkgs.nixfmt
+  environment.systemPackages = with pkgs; [
+    nixfmt
+    nixfmt-tree
   ];
 
   # automatically optimise the Nix store and enable automatic garbage collection
