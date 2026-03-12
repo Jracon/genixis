68b0741d9cba6519904400cf5d8b91d1716480f4
Jade Isaiah Meskill <jade.isaiah@gmail.com>
2026-03-12 11:58:13 -0700

    fix packages for real this time

--- common/nixos.nix
+++ common/nixos.nix
@@ -11,6 +11,9 @@
     grub.configurationLimit = 5;
     systemd-boot.configurationLimit = 5;
   };
+  environment.systemPackages = [
+    pkgs.isd
+  ];
   programs = {
     nix-ld.enable = true;
     zsh.enable = true;
