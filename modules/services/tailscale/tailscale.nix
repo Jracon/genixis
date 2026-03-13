519ca5dc0b7a51bb7ff298a416cc8d2dd9aaeb6f
Jade Isaiah Meskill <jade.isaiah@gmail.com>
2026-03-13 15:18:19 -0700

    improve tailscale configuration

--- modules/services/tailscale/tailscale.nix
+++ modules/services/tailscale/tailscale.nix
@@ -7,10 +7,23 @@
 {
   age.secrets.tailscale_client_secret.file = ./client_secret.age;
 
+  boot.initrd.systemd.network.wait-online.enable = false;
+
   environment.systemPackages = with pkgs; [
     ethtool
     networkd-dispatcher
   ];
+
+  networking.firewall = {
+    enable = true;
+
+    allowedUDPPorts = [
+      config.services.tailscale.port
+    ];
+    trustedInterfaces = [
+      "tailscale0"
+    ];
+  };
 
   services = {
     networkd-dispatcher = {
@@ -39,5 +52,13 @@
         "--advertise-routes=10.0.0.0/24"
       ];
     };
+  };
+
+  systemd = {
+    network.wait-online.enable = false;
+
+    services.tailscaled.serviceConfig.Environment = [
+      "TS_DEBUG_FIREWALL_MODE=nftables"
+    ];
   };
 }
