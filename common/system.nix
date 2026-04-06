dc084c165d3fd66e71d1bcf7e850444e20bbd67a
Jade Isaiah Meskill <jade.isaiah@gmail.com>
2026-04-06 16:19:22 -0700

    stop replacing nix/nix.conf

--- common/system.nix
+++ common/system.nix
@@ -2,8 +2,4 @@
   nixpkgs.hostPlatform = "x86_64-linux";
   nix.settings.auto-optimise-store = true;
   system-manager.allowAnyDistro = true;
-
-  environment.etc = {
-    "nix/nix.conf".replaceExisting = true;
-  };
 }
