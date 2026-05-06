ffa04c6eaba08fc01b5f21c6005541f2c930680d
Jade Isaiah Meskill <jade.isaiah@gmail.com>
2026-05-05 20:26:07 -0700

    formatting

--- modules/services/rclone-webdav.nix
+++ modules/services/rclone-webdav.nix
@@ -6,9 +6,13 @@
 }:
 
 {
-  environment.systemPackages = [ pkgs.rclone ];
+  environment.systemPackages = [
+    pkgs.rclone
+  ];
 
-  networking.firewall.allowedTCPPorts = [ 8080 ];
+  networking.firewall.allowedTCPPorts = [
+    8080
+  ];
 
   system.activationScripts.create_rclone_webdav_directory.text = ''
     mkdir -p /mnt/media/games/saves/
@@ -16,11 +20,17 @@
 
   systemd.services.rclone-webdav = {
     description = "rclone WebDAV server";
-    after = [ "network.target" ];
-    wantedBy = [ "multi-user.target" ];
 
+    after = [
+      "network.target"
+    ];
     serviceConfig = {
       Type = "simple";
+      DynamicUser = true;
+      Restart = "on-failure";
+      RestartSec = "5s";
+      StateDirectory = "rclone-webdav";
+
       ExecStart = lib.concatStringsSep " " [
         "${pkgs.rclone}/bin/rclone serve webdav"
         # TODO: set your remote or local path, e.g. "myremote:" or "/mnt/media"
@@ -30,10 +40,9 @@
         # "--config ${config.age.secrets.rclone_config.path}"
         # "--htpasswd ${config.age.secrets.rclone_htpasswd.path}"
       ];
-      Restart = "on-failure";
-      RestartSec = "5s";
-      DynamicUser = true;
-      StateDirectory = "rclone-webdav";
     };
+    wantedBy = [
+      "multi-user.target"
+    ];
   };
 }
