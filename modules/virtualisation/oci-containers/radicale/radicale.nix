a4ac7fefe406781d8942b625f5d7f39479baa253
Jade Isaiah Meskill <jade.isaiah@gmail.com>
2026-08-12 10:57:47 -0700

    revert filepath changes

--- modules/virtualisation/oci-containers/radicale/radicale.nix
+++ modules/virtualisation/oci-containers/radicale/radicale.nix
@@ -28,8 +28,8 @@
     ];
     volumes = [
       "/mnt/radicale/config:/etc/radicale"
-      "${config.age.secrets.radicale_config.path}:/etc/radicale/config/config"
-      "${config.age.secrets.radicale_users.path}:/etc/radicale/config/users"
+      "${config.age.secrets.radicale_config.path}:/etc/radicale/config"
+      "${config.age.secrets.radicale_users.path}:/etc/radicale/users"
       "/mnt/radicale/data:/var/lib/radicale"
     ];
   };
